179dc872e2a499bb37d0ae93dfd3b987bf425deb
cleger97 <chris.legerjr@gmail.com>
2017-01-19 15:32:47 -0500

    Starting the website
    Beginning to write the base code for the website.

---
+++ showall.php
@@ -0,0 +1,21 @@
+<!--
+	File: showall.php
+	Dev: Alex Leger
+	Date Stated: 2017/1/19
+	Description: The site that displays all the movies.
+-->
+<?php
+	include("./data/dbinc");
+	$sort_options = array( "title" => "Title", "year" => "Year", "genre" => "Genre", "rating" => "Rating", "actorList" => "Actor List");
+?>
+<html>
+
+<head>
+<title> Movie List </title>
+</head>
+
+<body>
+
+</body>
+
+</html>
