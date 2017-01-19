a0ad2c925b56bfd4a4d96ae244bf26c5fd56cf50
cleger97 <chris.legerjr@gmail.com>
2017-01-19 15:36:56 -0500

    Adjusting files
    Just changing some small things.

--- showall.php
+++ showall.php
@@ -5,7 +5,7 @@
 	Description: The site that displays all the movies.
 -->
 <?php
-	include("./data/dbinc");
+	include("./data/dbinc.incf");
 	$sort_options = array( "title" => "Title", "year" => "Year", "genre" => "Genre", "rating" => "Rating", "actorList" => "Actor List");
 ?>
 <html>
